429eb0f58ca40403feb7d3caa06a2dcc487fa40b
nick-jia <44851859+nick-jia@users.noreply.github.com>
2020-11-10 14:48:16 -0500

    Update prepare_data.py

--- datasets/purchase/prepare_data.py
+++ datasets/purchase/prepare_data.py
@@ -2,9 +2,10 @@ import os
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
+from scipy.sparse import load_npz
 
 
-data = np.load('data.npy')
+data = np.concatenate([load_npz('data1.npz').toarray(), load_npz('data2.npz').toarray()]).astype(int)
 
 num_class = 2
 
@@ -19,4 +20,3 @@ if not os.path.exists(f'purchase{num_class}_train.npy'):
     X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.2)
     np.save(f'purchase{num_class}_train.npy', {'X': X_train, 'y': y_train})
     np.save(f'purchase{num_class}_test.npy', {'X': X_test, 'y': y_test})
-
